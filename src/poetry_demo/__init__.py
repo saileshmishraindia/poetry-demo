8f4431b5a25912688fbd9d0772bce5389784d6cf
Sailesh Mishra <saileshmishra164@gmail.com>
2025-09-19 14:43:00 +0530

    fix: remove extra space in greet()

--- src/poetry_demo/__init__.py
+++ src/poetry_demo/__init__.py
@@ -2,7 +2,7 @@ import pendulum
 
 def greet():
     now = pendulum.now()
-    return f"Hello ! The current time is {now.to_day_datetime_string()}"
+    return f"Hello! The current time is {now.to_day_datetime_string()}"
 
 def days_ahead(days: int):
     now = pendulum.now()
