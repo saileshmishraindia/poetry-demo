532b9c761b228a7903dea56d7dd69744600421d1
Sailesh Mishra <saileshmishra164@gmail.com>
2025-09-19 11:56:03 +0530

    Initial commit: Poetry demo project

---
+++ src/poetry_demo/__init__.py
@@ -0,0 +1,11 @@
+import pendulum
+
+def greet():
+    now = pendulum.now()
+    return f"Hello ! The current time is {now.to_day_datetime_string()}"
+
+def days_ahead(days: int):
+    now = pendulum.now()
+    future = now.add(days=days)
+    return f"In {days} days it will be {future.to_day_datetime_string()}"
+
